d872c8ae50e659f0689f65658adf870311ebd38e
Michal Wojciechowski <odyniec@cpan.org>
2014-09-06 23:44:40 +0200

    Add support for dist_file, module_dir, and module_file

--- lib/File/ShareDir/Override.pm
+++ lib/File/ShareDir/Override.pm
@@ -9,7 +9,8 @@ use warnings;
 
 use File::ShareDir;
 
-my %dist_dirs = ();
+my %dist_dirs;
+my %module_dirs;
 
 sub import {
     my ($package, $dir) = @_;
@@ -18,8 +19,26 @@ sub import {
     
     if ($dir =~ /:/) {
         map {
-            my ($dist, $dir) = split ':', $_;
-            $dist_dirs{$dist} = $dir;
+            my @opt = split /(?<!:):(?!:)/, $_;
+
+            if ($opt[0] =~ /::/) {
+                # module_dir ("Foo::Bar:/some/path")
+                $module_dirs{$opt[0]} = $opt[2] || $opt[1];
+            }
+            elsif (defined $opt[2]) {
+                if ($opt[1] eq 'dist') {
+                    # dist_dir ("Foo:dist:/some/path")
+                    $dist_dirs{$opt[0]} = $opt[2];
+                }
+                elsif ($opt[1] eq 'module') {
+                    # module_dir ("Foo:module:/some/path")
+                    $module_dirs{$opt[0]} = $opt[2];
+                }
+            }
+            else {
+                # Assume dist_dir ("Foo-Bar:/some/path" or "Foo:/some/path")
+                $dist_dirs{$opt[0]} = $opt[1];
+            }
         } split ',', $dir;
     }
     else {
@@ -29,13 +48,47 @@ sub import {
 
 {
     my $_File_ShareDir_dist_dir = \&File::ShareDir::dist_dir;
+    my $_File_ShareDir_dist_file = \&File::ShareDir::dist_file;
+    my $_File_ShareDir_module_dir = \&File::ShareDir::module_dir;
+    my $_File_ShareDir_module_file = \&File::ShareDir::module_file;
 
     no strict 'refs';
     no warnings 'redefine';
+
     *{"File::ShareDir::dist_dir"} = sub {
         my $dist = File::ShareDir::_DIST(shift);
 
         return $dist_dirs{$dist} || &$_File_ShareDir_dist_dir($dist);
+    };
+
+    *{"File::ShareDir::dist_file"} = sub {
+        my $dist = File::ShareDir::_DIST(shift);
+        my $file = File::ShareDir::_FILE(shift);
+
+        if ($dist_dirs{$dist}) {
+            return File::Spec->catfile($dist_dirs{$dist}, $file);
+        }
+        else {
+            return &$_File_ShareDir_dist_file($dist, $file);
+        }
+    };
+
+    *{"File::ShareDir::module_dir"} = sub {
+        my $module = File::ShareDir::_MODULE(shift);
+
+        return $module_dirs{$module} || &$_File_ShareDir_module_dir($module);
+    };
+    
+    *{"File::ShareDir::module_file"} = sub {
+        my $module = File::ShareDir::_MODULE(shift);
+        my $file = File::ShareDir::_FILE(shift);
+
+        if ($module_dirs{$module}) {
+            return File::Spec->catfile($module_dirs{$module}, $file);
+        }
+        else {
+            return &$_File_ShareDir_module_file($module, $file);
+        }
     };
 }
 
@@ -45,6 +98,21 @@ __END__
 
 =head1 SYNOPSIS
 
-Run C<program.pl> and make Foo::Bar think its share directory is C<./share>:
+Run C<program.pl> and make Foo::Bar think its distribution's shared directory is
+C<./share>:
     
     perl -MFile::ShareDir::Override=Foo-Bar:./share program.pl
+
+Pretend Foo::Bar's module shared directory is C<./lib>:
+
+    perl -MFile::ShareDir::Override=Foo::Bar:./lib program.pl
+
+=head1 DESCRIPTION
+
+(TBA)
+
+Top-level modules/distributions (e.g., Dancer or Plack) don't have any dashes or
+double colons in the name and can't be recognized, so they need an explicit
+C<:dist> or C<:module> between the name and path. Example:
+
+    perl -MFile::ShareDir::Override=Foo:module:./lib program.pl
